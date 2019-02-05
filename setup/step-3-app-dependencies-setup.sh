3ea23fafb8c9cc847105da366da569662532088f
Phil Chen <nethacker@gmail.com>
2019-02-04 20:45:36 -0800

    Added copy of app to virtualenv directory

--- setup/step-3-app-dependencies-setup.sh
+++ setup/step-3-app-dependencies-setup.sh
@@ -5,4 +5,6 @@ cd /home/ubuntu/flask_apps
 virtualenv app01_env
 source app01_env/bin/activate
 pip install gunicorn flask
+cp /home/ubuntu/scalable-ubuntu-flask-gunicorn-nginx/flask_app/app01.py /home/ubuntu/flask_apps/app01_env/
+cp /home/ubuntu/scalable-ubuntu-flask-gunicorn-nginx/flask_app/wsgi.py /home/ubuntu/flask_apps/app01_env/
 deactivate
