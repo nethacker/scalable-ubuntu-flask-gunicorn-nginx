5b3c2bc08d974360bddc20f884bed992132a6c4f
Phil Chen <nethacker@gmail.com>
2019-02-05 13:50:17 -0800

    adding python module setproctitle to show process name

--- setup/step-3-app-dependencies-setup.sh
+++ setup/step-3-app-dependencies-setup.sh
@@ -5,6 +5,7 @@ cd /home/ubuntu/flask_apps
 virtualenv app01_env
 source app01_env/bin/activate
 pip install gunicorn flask
+pip install setproctitle
 cp /home/ubuntu/scalable-ubuntu-flask-gunicorn-nginx/flask_app/app01.py /home/ubuntu/flask_apps/app01_env/
 cp /home/ubuntu/scalable-ubuntu-flask-gunicorn-nginx/flask_app/wsgi.py /home/ubuntu/flask_apps/app01_env/
 cp /home/ubuntu/scalable-ubuntu-flask-gunicorn-nginx/gunicorn/gunicorn_config.py /home/ubuntu/flask_apps/app01_env/
