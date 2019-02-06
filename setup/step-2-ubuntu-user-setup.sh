9b03d790f59b3448e7aa72a8249e0b2d14270344
Phil Chen <nethacker@gmail.com>
2019-02-05 21:55:16 -0800

    Combining setup scripts and updating readme

--- setup/step-2-ubuntu-user-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo '# default location of virtual environment directories' >> ~/.bashrc
-echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.bashrc
-echo '# default python version to use with virtualenv' >> ~/.bashrc
-echo 'export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3' >> ~/.bashrc
-echo "export VIRTUALENVWRAPPER_VIRTUALENV_ARGS=' -p /usr/bin/python3 '" >> ~/.bashrc
-echo 'source `which virtualenvwrapper.sh`' >> ~/.bashrc
-source ~/.bashrc
