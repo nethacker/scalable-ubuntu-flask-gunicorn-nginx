e6c60645ed39eb8479b8e5186d5190439cbbff27
Phil Chen <nethacker@gmail.com>
2019-02-11 14:04:17 -0800

    Adding nginx config default and remove default site

--- setup/setup.sh
+++ setup/setup.sh
@@ -44,5 +44,6 @@ sudo systemctl enable app01.service
 # nginx setup
 
 sudo cp /home/ubuntu/scalable-ubuntu-flask-gunicorn-nginx/nginx/nginx_app01.conf /etc/nginx/sites-available/
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/nginx_app01.conf /etc/nginx/sites-enabled
 sudo systemctl restart nginx
